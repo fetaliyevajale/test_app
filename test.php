183de1f50a8b4a4af3478b61f7d4ba796f75ef58
fetaliyevajale <fetaliyevajale@gmail.com>
2024-07-10 10:24:13 -0700

    first commit

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST['name'];
+$surname = $_POST['pasüord'];
+$age = $_POST['age'];
+
+echo "name: " . $name . "<br>";
+echo "surname: " . $surname . "<br>";
+echo "age: " . $age . "<br>";
+?>
